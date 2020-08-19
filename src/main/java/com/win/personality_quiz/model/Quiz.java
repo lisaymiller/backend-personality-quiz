32e1bc2c3252e52ac04ef275907cb5ba359dca3a
Lisa Miller <lisayevonne@outlook.com>
2020-08-19 09:05:53 -0400

    test

--- src/main/java/com/win/personality_quiz/model/Quiz.java
+++ src/main/java/com/win/personality_quiz/model/Quiz.java
@@ -46,10 +46,6 @@ public class Quiz {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getQuestion() {
         return question;
     }
