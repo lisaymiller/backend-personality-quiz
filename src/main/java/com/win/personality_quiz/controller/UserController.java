bdcb4f645258211bdbee9b07e14130289195eed6
Lisa Miller <lisayevonne@outlook.com>
2020-08-19 09:07:57 -0400

    test

--- src/main/java/com/win/personality_quiz/controller/UserController.java
+++ src/main/java/com/win/personality_quiz/controller/UserController.java
@@ -6,10 +6,8 @@ import com.win.personality_quiz.model.User;
 import com.win.personality_quiz.repository.UserRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
